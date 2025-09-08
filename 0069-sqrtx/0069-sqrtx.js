87f9aa0cdd248b77d57d8b198f2abd1cf63bade9
Jayanth P Shetty <57756465+jayanthp24@users.noreply.github.com>
2025-09-08 23:34:26 +0530

    Time: 1 ms (71.96%), Space: 54.1 MB (99.22%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+    if (x < 2) return x;
+    let left = 2;
+    let right = Math.floor(x / 2);
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (x == mid * mid) {
+            return mid;
+        } else if (x < mid * mid) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return right;
+};
