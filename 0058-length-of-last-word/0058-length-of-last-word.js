91ab6f2c6c227592008481ebc9e3324a9dcd236d
Jayanth P Shetty <57756465+jayanthp24@users.noreply.github.com>
2026-01-13 00:14:41 +0530

    Time: 0 ms (100%), Space: 54.2 MB (37.65%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+    let n = s.length - 1;
+    while (n >= 0 && s[n] === ' ') n--;
+
+    let count = 0;
+    while (n >= 0 && s[n] != ' ') {
+        count++;
+        n--;
+    }
+    return count;
+};
