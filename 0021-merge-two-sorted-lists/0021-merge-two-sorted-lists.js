ed1aec4402a47aab9f1ec1a97a7631c1fb85689e
Jayanth P Shetty <57756465+jayanthp24@users.noreply.github.com>
2026-01-13 00:11:34 +0530

    Time: 0 ms (100%), Space: 58.3 MB (12.6%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+    let dummy = new ListNode();
+    let dummyHead = dummy;
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            dummy.next = list1;
+            list1 = list1.next;
+        } else {
+            dummy.next = list2;
+            list2 = list2.next;
+        }
+        dummy = dummy.next;
+    }
+    dummy.next = !list1 ? list2 : list1;
+    return dummyHead.next;
+};
