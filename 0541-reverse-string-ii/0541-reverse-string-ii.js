7592705f8de9b9e46375c307cd830cc426fed50f
Jayanth P Shetty <57756465+jayanthp24@users.noreply.github.com>
2026-01-13 00:19:05 +0530

    Time: 5 ms (58.2%), Space: 56 MB (67.3%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function (s, k) {
+    s = s.split("");
+
+    for (let x = 0; x < s.length; x = x + (2 * k)) {
+        let n = k;
+        let mid = Math.floor(k / 2);
+        for (let i = 0; i < mid; i++) {
+            let temp = s[x + i];
+            s[x + i] = s[x + n - 1 - i];
+            s[x + n - i - 1] = temp;
+        }
+    }
+
+    return s.join('');
+};
