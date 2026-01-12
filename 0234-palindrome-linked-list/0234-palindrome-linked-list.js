2d2eabc8d59aa9716a039f9c9aa256ea852b8e1c
Jayanth P Shetty <57756465+jayanthp24@users.noreply.github.com>
2026-01-13 00:07:36 +0530

    Time: 3 ms (91.91%), Space: 76.9 MB (60.72%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    // Finding the middle element
+    let slow = head;
+    let fast = head;
+    while(fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    let middle = slow;
+
+    // Reverse the second half of list
+    let prev = null;
+    let curr = middle;
+    while(curr) {
+        let temp = curr.next
+        curr.next = prev;
+        prev = curr;
+        curr = temp;
+    }
+    let secondHead = prev;
+
+    // Checking for palindrome
+    let firstList = head;
+    let secondList = secondHead;
+    while(secondList) {
+        if(firstList.val != secondList.val) {
+            return false;
+        }
+        firstList = firstList.next;
+        secondList = secondList.next;
+    }
+    return true;
+};
